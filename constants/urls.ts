45ff9b5876e96c66c1a2005ce86d7e349cbdf8a6
Lidia Batinas <lbatinas@yonder.local>
2025-09-22 15:10:33 +0300

    add tests for login page

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,4 +1,5 @@
 export const urls = {
   localhost: 'http://localhost:4200/',
   dashboardPage: 'http://localhost:4200/pages/iot-dashboard',
+  authLoginPage: 'http://localhost:4200/auth/login',
 };
--- tests/form-layouts.spec.ts
+++ tests/form-layouts.spec.ts
@@ -3,7 +3,7 @@ import { InputHelpers } from '../helpers/InputHelpers';
 import { gridForm } from '../constants/selectors';
 import { basicForm } from '../constants/selectors';
 import { urls } from '../constants/urls';
-import { email, password } from '../constants/data';
+import { email, password } from '../test-data/inputData';
 
 //navigate to Forms page
 test.beforeEach(async ({ page }) => {
@@ -23,45 +23,25 @@ test('verify element is present, visible, editable', async ({ page }) => {
 // GRID FORMS TESTS
 test.describe('Checks grid forms', () => {
   test('Validate email input', async ({ page }) => {
-    // Instantiate the utils class
-    const inputHelpers = new InputHelpers();
-    const emailInput = page.locator(gridForm.emailGridForm);
-
-    await emailInput.fill(email);
-    // call the method we defined on the utils class
-    await inputHelpers.assertInputValue(emailInput, email);
+    await InputHelpers.setInputValue(page, gridForm.emailGridForm, email);
+    await InputHelpers.assertInputValue(page, gridForm.emailGridForm, email);
   });
 
   test('Validate password input', async ({ page }) => {
-    // Instantiate the utils class
-    const inputHelpers = new InputHelpers();
-    const passwordInput = page.locator(gridForm.passwordGridForm);
-
-    await passwordInput.fill(password);
-    // call the method we defined on the utils class
-    await inputHelpers.assertInputValue(passwordInput, password);
+    await InputHelpers.setInputValue(page, gridForm.passwordGridForm, email);
+    await InputHelpers.assertInputValue(page, gridForm.passwordGridForm, email);
   });
 });
 
 // BASIC FORMS
 test.describe('Checks basic forms', () => {
   test('Validate email input', async ({ page }) => {
-    // Instantiate the utils class
-    const inputHelpers = new InputHelpers();
-    const emailInput = page.locator(basicForm.emailBasicForms);
-
-    await emailInput.fill(email);
-    // call the method we defined on the utils class
-    await inputHelpers.assertInputValue(emailInput, email);
+    await InputHelpers.setInputValue(page, basicForm.emailBasicForms, email);
+    await InputHelpers.assertInputValue(page, basicForm.emailBasicForms, email);
   });
 
   test('Validate password input', async ({ page }) => {
-    // Instantiate the utils class
-    const inputHelpers = new InputHelpers();
-    const passwordInput = page.locator(basicForm.passwordBasicForms);
-
-    await passwordInput.fill(password);
-    // call the method we defined on the utils class
-    await inputHelpers.assertInputValue(passwordInput, password);
+    await InputHelpers.setInputValue(page, basicForm.passwordBasicForms, email);
+    await InputHelpers.assertInputValue(page, basicForm.passwordBasicForms, email);
   });
 });
